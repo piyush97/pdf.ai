bed758ba51435b34df2c2ceabbdcfe31fd5fad3a
piyush97 <me@piyushmehta.com>
2023-10-01 15:28:52 -0400

    Lucide and button

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PDF_AI = "PDF.ai";
+export const GO_TO_CHATS = "Go to Chats";
+export const HOME_SUBTITLE =
+  "Join students, researchers and professionals like you who instantly find answers from the PDF they upload.";
+export const MADE_WITH_LOVE = " Made with 🖤 by Piyush Mehta";
