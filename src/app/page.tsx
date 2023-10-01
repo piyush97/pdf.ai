45be3df147ea8f99d3493bc040424a4b8a6a012a
piyush97 <me@piyushmehta.com>
2023-10-01 14:54:21 -0400

    Shadcn Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
 export default async function Home() {
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-teal-100 to-rose-100">
@@ -5,6 +8,10 @@ export default async function Home() {
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="mr-3 text-5xl font-semibold">PDF.ai</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            <Button>Go to Chats</Button>
           </div>
         </div>
       </div>
