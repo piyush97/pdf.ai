d61a76235fbefe4c4e764bb9e96bbf82da21428f
piyush97 <me@piyushmehta.com>
2023-10-30 23:43:49 -0400

    db config

---
+++ src/lib/db/index.ts
@@ -0,0 +1,12 @@
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+neonConfig.fetchConnectionCache = true;
+
+if (!process.env.DB_URL) {
+  throw new Error("Db URL not found in env variables");
+}
+
+const sql = neon(process.env.DB_URL);
+
+export const db = drizzle(sql);
