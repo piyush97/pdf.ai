b97ca9514d60875100b207666c928dbf842b90e1
piyush97 <me@piyushmehta.com>
2023-10-30 22:05:44 -0400

    Just page css center

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 const Page = () => {
   return (
-    <div>
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <SignIn />
     </div>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 const Page = () => {
   return (
-    <div>
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <SignUp />
     </div>
   );
