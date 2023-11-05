21dd2ecdd111d24bef306e1e7a963999d10c157d
piyush97 <me@piyushmehta.com>
2023-11-05 17:38:04 -0500

    Upload functionality

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { PDF_DROP_HELPER } from "@/lib/constants";
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 
@@ -7,8 +8,19 @@ const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("File should be lesser than 10Mb!");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log("data", data);
+      } catch (error) {
+        console.error(error);
+      }
     },
   });
 
