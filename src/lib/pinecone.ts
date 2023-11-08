ceebc4e09e07a72a2fa1af1bf4d955980d40582e
piyush97 <me@piyushmehta.com>
2023-11-07 22:36:43 -0500

    Loading langchain and sending as a response

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,4 +1,5 @@
 import { Pinecone } from "@pinecone-database/pinecone";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { downloadFromS3 } from "./s3-server";
 
 export const pinecone = new Pinecone({
@@ -7,6 +8,12 @@ export const pinecone = new Pinecone({
 });
 
 export const loadS3IntoPinecone = async (fileKey: string) => {
-  console.log("downloading s3 into file system");
+  console.log("downloading s3 file");
   const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("Could not download file from S3");
+  }
+  const loader = new PDFLoader(file_name);
+  const pages = loader.load();
+  return pages;
 };
